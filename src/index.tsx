a8dc7b31d02390d1051bdbb31c78c1499cca37b7
mohamedderaz42-cpu <mohamedderaz42@gmail.com>
2025-11-22 03:28:55 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ErrorBoundary } from './components/ErrorBoundary';
 import { LanguageProvider } from './core/i18n/LanguageContext';
+// 1. استيراد المزود الجديد
+import { ToastProvider } from './components/ui/ToastProvider';
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+    throw new Error("Could not find root element to mount to");
 }
 
 const root = ReactDOM.createRoot(rootElement);
@@ -15,7 +16,10 @@ root.render(
   <React.StrictMode>
     <ErrorBoundary>
       <LanguageProvider>
-        <App />
+        {/* 2. تغليف التطبيق بالمزود هنا */}
+        <ToastProvider>
+          <App />
+        </ToastProvider>
       </LanguageProvider>
     </ErrorBoundary>
   </React.StrictMode>
